8d1725f1c77ba8ab30d7b7fe9a695ccad617bc1d
Thomas Slater <thomasslatergg@gmail.com>
2026-03-25 20:42:58 +0000

    Initial Commit

---
+++ Roost_iOS/Roost_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roost_iOSApp.swift
+//  Roost_iOS
+//
+//  Created by Tom Slater on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Roost_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
